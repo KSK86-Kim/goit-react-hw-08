19a21452a6f66ad1d1e13b8f3b0795b863f9a58b
Oleksandr Polishchuk <70407461+KSK86-Kim@users.noreply.github.com>
2021-06-20 08:20:33 +0300

    Updates

---
+++ static/js/home-view.65b25a77.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08"]=this["webpackJsonpgoit-react-hw-08"]||[]).push([[1],{113:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return i}));n(0);var c=n(1);function i(){return Object(c.jsx)("div",{className:"container",children:Object(c.jsx)("h1",{children:"PhoneBook main page"})})}}}]);
+//# sourceMappingURL=home-view.65b25a77.chunk.js.map
